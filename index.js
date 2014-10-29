80c6483b0e721e8dbb0ea45ccc933cac957cebcf
fraserxu <xvfeng123@gmail.com>
2014-10-29 16:42:37 +0800

    first release

---
+++ index.js
@@ -0,0 +1,30 @@
+'use strict'
+
+var React = require('react')
+var Chartist = require('chartist')
+
+var ChartistGraph = React.createClass({
+
+  displayName: 'ChartistGraph',
+
+  propTypes: {
+    type: React.PropTypes.string.isRequired,
+    data: React.PropTypes.object.isRequired,
+    options: React.PropTypes.object,
+    responsiveOptions: React.PropTypes.array
+  },
+
+  componentDidMount: function() {
+    var type = this.props.type
+    var data = this.props.data
+    var options = this.props.options || {}
+    var responsiveOptions = this.props.responsiveOptions || []
+    new Chartist[type]('.ct-chart', data, options, responsiveOptions)
+  },
+
+  render: function() {
+    return React.DOM.div({className: 'ct-chart'})
+  }
+})
+
+module.exports = ChartistGraph
