cf2bb7f79aaf5d05238242168fc37d04f457a806
Amit Shah <amit@amwam.me>
2018-12-14 11:24:30 +0000

    Fix types

--- index.d.ts
+++ index.d.ts
@@ -1,12 +1,14 @@
 import * as React from 'react';
+import { IChartOptions } from 'chartist';
 
 export interface ChartitGraphProps {
   type: 'Line' | 'Bar' | 'Pie';
   data: object;
   className?: string;
   options?: IChartOptions;
-  responsiveOptions?: Array<IResponsiveOptionTuple>;
+  responsiveOptions?: Array;
   style?: React.CSSProperties;
 }
 
 export class ChartistGraph extends React.Component<ChartitGraphProps> {}
+export default ChartistGraph;
