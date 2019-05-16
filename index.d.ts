ca7c0055c7b272e1c78e3831e7a1092f609b68cd
Benjamin Dobell <benjamin.dobell+git@glassechidna.com.au>
2019-05-17 05:33:45 +1000

    Fixed TypeScript definitions

--- index.d.ts
+++ index.d.ts
@@ -1,37 +1,48 @@
-import { ChartitGraphProps } from './index.d';
-import * as React from 'react';
+import * as React from 'react'
+
 import {
-  IChartOptions,
-  IResponsiveOptionTuple,
-  ILineChartOptions,
   IBarChartOptions,
+  ICandleChartOptions,
+  IChartOptions,
+  ILineChartOptions,
   IPieChartOptions,
-} from 'chartist';
+  IResponsiveOptionTuple,
+} from 'chartist'
 
-export interface ChartitGraphProps {
-  type: string;
-  data: object;
-  className?: string;
-  options?: IChartOptions;
-  style?: React.CSSProperties;
+export type ChartistListenerOptions = {[key: string]: Function}
+
+interface BaseChartistGraphProps {
+  data: Object
+  className?: string
+  options?: IChartOptions
+  style?: React.CSSProperties
+  listener?: ChartistListenerOptions
 }
 
-export interface ChartitGraphLineProps extends ChartitGraphProps {
-  type: 'Line';
-  options?: ILineChartOptions;
-  responseOptions?: Array<IResponsiveOptionTuple<ILineChartOptions>>;
+export interface ChartistGraphBarProps extends BaseChartistGraphProps {
+  type: 'Bar'
+  options: IBarChartOptions
+  responsiveOptions?: Array<IResponsiveOptionTuple<IBarChartOptions>>
 }
 
-export interface ChartitGraphPieProps extends ChartitGraphProps {
-  type: 'Pie';
-  options?: IPieChartOptions;
-  responseOptions?: Array<IResponsiveOptionTuple<IPieChartOptions>>;
+export interface ChartistGraphCandleProps extends BaseChartistGraphProps {
+  type: 'Candle'
+  options: ICandleChartOptions
+  responsiveOptions?: Array<IResponsiveOptionTuple<ICandleChartOptions>>
 }
 
-export interface ChartitGraphBarProps extends ChartitGraphProps {
-  type: 'Bar';
-  options: IBarChartOptions;
-  responseOptions?: Array<IResponsiveOptionTuple<IBarChartOptions>>;
+export interface ChartistGraphLineProps extends BaseChartistGraphProps {
+  type: 'Line'
+  options?: ILineChartOptions
+  responsiveOptions?: Array<IResponsiveOptionTuple<ILineChartOptions>>
 }
 
-export default class ChartistGraph extends React.Component<ChartitGraphProps> {}
+export interface ChartistGraphPieProps extends BaseChartistGraphProps {
+  type: 'Pie'
+  options?: IPieChartOptions
+  responsiveOptions?: Array<IResponsiveOptionTuple<IPieChartOptions>>
+}
+
+export type ChartistGraphProps = ChartistGraphBarProps | ChartistGraphCandleProps | ChartistGraphLineProps | ChartistGraphPieProps
+
+export default class ChartistGraph extends React.Component<ChartistGraphProps> {}
